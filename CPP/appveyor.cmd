44955d2f3514b8d14d3fc05c12d88a355f7cd1ef
Tino Reichardt <milky-7zip@mcmilk.de>
2020-06-06 13:50:07 +0200

    geändert:       appveyor.cmd

--- CPP/appveyor.cmd
+++ CPP/appveyor.cmd
@@ -5,7 +5,7 @@ REM Microsoft Visual Studio 2010 (VC=10.0) -> for win2k, but who needs that?
 REM Microsoft Visual Studio 2012 (VC=11.0)
 REM Microsoft Visual Studio 2013 (VC=12.0)
 REM Microsoft Visual Studio 2015 (VC=14.0)
-REM Microsoft Visual Studio 2019 (VC=15.0)
+REM Microsoft Visual Studio 2017 (VC=15.0)
 REM Microsoft Visual Studio 2019 (VC=16.0)
 
 REM Microsoft Visual Studio 2019 /SUBSYSTEM version numbers:
